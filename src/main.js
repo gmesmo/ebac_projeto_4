8b16b72919eb8b90009ee889c528130eabb99a5e
GMesmo <guimachado94@gmail.com>
2025-02-07 16:49:16 -0300

    implementação do resto do site

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll("[data-tab-button]");
+  const questions = document.querySelectorAll("[data-faq-question]");
+
+  const heroSection = document.querySelector(".hero");
+  const alturaHero = heroSection.clientHeight;
+
+  window.addEventListener("scroll", function () {
+    const posicaoAtual = window.scrollY;
+
+    if (posicaoAtual < alturaHero) {
+      ocultaElementosDoHeader();
+    } else {
+      exibeElementosDoHeader();
+    }
+  });
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function (botao) {
@@ -11,8 +25,19 @@ document.addEventListener("DOMContentLoaded", () => {
       aba.classList.add("shows__list--is-active");
       botao.target.classList.add("shows__tabs__button--is-active");
     });
+
+    for (let i = 0; i < questions.length; i++) {
+      questions[i].addEventListener("click", abreOuFechaResposta);
+    }
   }
 });
+
+function abreOuFechaResposta(elemento) {
+  const classe = "faq__questions__item--is-open";
+  const elementoPai = elemento.target.parentNode;
+
+  elementoPai.classList.toggle(classe);
+}
 
 function removeBotoesAtivos() {
   const buttons = document.querySelectorAll("[data-tab-button]");
@@ -28,4 +53,14 @@ function escondeTodasAsTabs() {
   for (let i = 0; i < tabsContainer.length; i++) {
     tabsContainer[i].classList.remove("shows__list--is-active");
   }
+}
+
+function ocultaElementosDoHeader() {
+  const header = document.querySelector("header");
+  header.classList.add("header--is-hidden");
+}
+
+function exibeElementosDoHeader() {
+  const header = document.querySelector("header");
+  header.classList.remove("header--is-hidden");
 }
